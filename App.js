da0ee8c83d31eb375187a234d1f3d0b580d25975
andi dirgantara <andi.dirgantara@MacBook-Pro-Travelokadmin.local>
2018-05-25 02:21:07 +0700

    basic function, scan barcode and fetch book info

--- App.js
+++ App.js
@@ -1,16 +1,41 @@
 import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Modal, Text, View, TouchableOpacity, TouchableHighlight } from 'react-native';
 import { Camera, Permissions } from 'expo';
 
 export default class CameraExample extends React.Component {
   state = {
     hasCameraPermission: null,
     type: Camera.Constants.Type.back,
+    modalVisible: false,
+    isbn: null,
+    result: null,
   };
 
   async componentWillMount() {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
     this.setState({ hasCameraPermission: status === 'granted' });
+  }
+
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
+
+  setIsbn(isbn) {
+    // expose API Key here for testing, subject to change
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}&key=AIzaSyC7I-Seo5oaYA-mtSOhqEpmoqqryrUeaYc`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => response.json())
+      .then((result) => {
+        this.setState({ isbn, modalVisible: true, result: result.items[0].volumeInfo.title });
+      })
+      .catch(err => {
+        this.setState({ isbn, modalVisible: true, result: err.toString() });
+      })
   }
 
   render() {
@@ -26,7 +51,7 @@ export default class CameraExample extends React.Component {
             style={{ flex: 1 }}
             type={this.state.type}
             onBarCodeRead={({ type, data }) => {
-              console.log(data)
+              this.setIsbn(data);
             }}
           >
             <View
@@ -55,6 +80,27 @@ export default class CameraExample extends React.Component {
               </TouchableOpacity>
             </View>
           </Camera>
+          <Modal
+            animationType="slide"
+            transparent={false}
+            visible={this.state.modalVisible}
+            onRequestClose={() => {
+              alert('Modal has been closed.');
+            }}>
+            <View style={{marginTop: 22}}>
+              <View>
+                <Text>{this.state.isbn}</Text>
+                <Text>{this.state.result}</Text>
+
+                <TouchableHighlight
+                  onPress={() => {
+                    this.setModalVisible(false);
+                  }}>
+                  <Text>Hide Modal</Text>
+                </TouchableHighlight>
+              </View>
+            </View>
+          </Modal>
         </View>
       );
     }
